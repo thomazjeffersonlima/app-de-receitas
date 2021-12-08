b9149c402b9f8eec7e4d32880826f0e057cefcb7
Guarize <pedroguarize@hotmail.com>
2021-12-08 17:11:26 -0300

    Req 12 - Completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,31 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 export default function Header({ title }) {
+  const [showSearchBar, setShowSearchBar] = useState(false);
+
   return (
     <div>
       <Link to="/perfil">
-        <img src={ profileIcon } alt="profile icon" data-testid="profile-top-btn" />
+        <input
+          type="image"
+          src={ profileIcon }
+          alt="profile icon"
+          data-testid="profile-top-btn"
+        />
       </Link>
       <h1 data-testid="page-title">{title}</h1>
-      <Link to="/">
-        <img src={ searchIcon } alt="search icon" data-testid="search-top-btn" />
-      </Link>
+      <input
+        type="image"
+        src={ searchIcon }
+        alt="search icon"
+        data-testid="search-top-btn"
+        onClick={ () => setShowSearchBar(!showSearchBar) }
+      />
+      {showSearchBar && <input type="text" data-testid="search-input" />}
     </div>
   );
 }
