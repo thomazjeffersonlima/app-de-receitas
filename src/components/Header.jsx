2d822b0e3755c4e92e38811a73db947f4c36a996
Guarize <pedroguarize@hotmail.com>
2021-12-08 16:43:06 -0300

    Req 9 - Completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
+
+export default function Header({ title }) {
+  return (
+    <div>
+      <Link to="/">
+        <img src={ profileIcon } alt="profile icon" data-testid="profile-top-btn" />
+      </Link>
+      <h1 data-testid="page-title">{title}</h1>
+      <Link to="/">
+        <img src={ searchIcon } alt="search icon" data-testid="search-top-btn" />
+      </Link>
+    </div>
+  );
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,5 +1,18 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import Header from '../components/Header';
 
-export default function Foods() {
-  return <div>Comidas</div>;
+export default function Foods({ location: { pathname } }) {
+  return (
+    <>
+      <Header title={ pathname.replace('/', '') } />
+      <div>Comidas</div>
+    </>
+  );
 }
+
+Foods.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string,
+  }).isRequired,
+};
