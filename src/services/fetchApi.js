1c7b7916cf663e94dfb22ed740592b70fbc6f7e8
Guarize <pedroguarize@hotmail.com>
2021-12-09 15:05:37 -0300

    [styles] basic login css - 2

--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -1,6 +1,7 @@
 export default async function fetchRecipies(type, endpoint) {
   if (type === 'Comidas') {
-    const foodApi = await fetch(`https://www.themealdb.com/api/json/v1/1/${endpoint}`).then((results) => results.json());
-    return foodApi.meals;
+    const { meals } = await fetch(`https://www.themealdb.com/api/json/v1/1/${endpoint}`)
+      .then((results) => results.json());
+    return meals;
   }
 }
