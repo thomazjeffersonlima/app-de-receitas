9b39fb7658ea6cf13e1c4a917196cf962c48aa65
thomazj <thomaz.jefferson01@protonmail.com>
2021-12-09 18:27:47 -0300

    req 19, 20 completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+import '../styles/Footer.css';
+
+export default function Footer() {
+  return (
+    <footer data-testid="footer">
+      <Link to="/bebidas">
+        <input
+          type="image"
+          src={ drinkIcon }
+          alt="drink icon"
+          data-testid="drinks-bottom-btn"
+        />
+      </Link>
+      <Link to="/explorar">
+        <input
+          type="image"
+          src={ exploreIcon }
+          alt="explore icon"
+          data-testid="explore-bottom-btn"
+        />
+      </Link>
+      <Link to="/comidas">
+        <input
+          type="image"
+          src={ mealIcon }
+          alt="food icon"
+          data-testid="food-bottom-btn"
+        />
+      </Link>
+    </footer>
+  );
+}
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Footer from '../components/Footer';
 
 export default function Details() {
   return (
-    <div>Details</div>
+    <Footer />
   );
 }
