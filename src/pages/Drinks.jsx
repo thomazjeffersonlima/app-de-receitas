0253fcc835b34c8e829d6c4daccaf044fab331e4
thomazj <thomaz.jefferson01@protonmail.com>
2021-12-09 19:21:35 -0300

    req 27 in progress

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,11 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import Header from '../components/Header';
 import RecipiesContext from '../contexts/RecipiesContext';
 import Footer from '../components/Footer';
+import fetchRecipies from '../services/fetchApi';
 
 export default function Drinks() {
-  const { drinksRecipies } = useContext(RecipiesContext);
+  const { drinksRecipies, setDrinksRecipies } = useContext(RecipiesContext);
   const maxLength = 12;
+
+  useEffect(() => {
+    async function responseApi() {
+      const returnDefaultFoods = await fetchRecipies('Bebidas', 'search.php?s=');
+      setDrinksRecipies(returnDefaultFoods);
+    }
+    responseApi();
+  }, []);
+
   return (
     <>
       <Header title="Bebidas" />
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,11 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import Header from '../components/Header';
 import RecipiesContext from '../contexts/RecipiesContext';
 import Footer from '../components/Footer';
+import fetchRecipies from '../services/fetchApi';
 
 export default function Foods() {
-  const { foodsRecipies } = useContext(RecipiesContext);
+  const { foodsRecipies, setFoodsRecipies } = useContext(RecipiesContext);
   const maxLength = 12;
+
+  useEffect(() => {
+    async function responseApi() {
+      const returnDefaultFoods = await fetchRecipies('Comidas', 'search.php?s=');
+      setFoodsRecipies(returnDefaultFoods);
+    }
+    responseApi();
+  }, []);
+
   return (
     <>
       <Header title="Comidas" />
