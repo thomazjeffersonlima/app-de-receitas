b124c597832fa6aecfa4c175f1c8fd07b26a2f69
thomazj <thomaz.jefferson01@protonmail.com>
2022-01-09 23:37:46 -0300

    requisitos

---
+++ src/components/RecipesDoneFilter.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function RecipesDoneFilter() {
+  return (
+    <div>
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+      >
+        Food
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+      >
+        Drinks
+      </button>
+    </div>
+  );
+}
--- src/pages/ReceitasFeitas.jsx
+++ src/pages/ReceitasFeitas.jsx
@@ -1,7 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import copy from 'clipboard-copy';
+import RecipesDoneFilter from '../components/RecipesDoneFilter';
+import shared from '../images/shareIcon.svg';
 
 export default function ReceitasFeitas() {
+  const [listObjectDoneRicipes, setListObjectDoneRicipes] = useState([]);
+
+  const handleShare = (id, type) => {
+    const typeAddS = type.concat('s');
+    const idConcat = `/${id}`;
+    const typeConcat = `/${typeAddS}`;
+    copy(window.location.origin + typeConcat + idConcat);
+    alert('Link copiado!');
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem('doneRecipes') !== undefined) {
+      setListObjectDoneRicipes(JSON.parse(localStorage.getItem('doneRecipes')));
+    }
+  }, []);
+
   return (
-    <div>Receitas Feitas</div>
+    <div>
+      <RecipesDoneFilter />
+      {
+        listObjectDoneRicipes.length > 0 && listObjectDoneRicipes.map(
+          ({
+            image,
+            category,
+            name,
+            doneDate,
+            tags,
+            alcoholicOrNot,
+            area,
+            id,
+            type,
+          }, i) => (
+            <div key={ i }>
+              <img src={ image } alt="" data-testid={ `${i}-horizontal-image` } />
+              <p data-testid={ `${i}-horizontal-top-text` }>{ category }</p>
+              {
+                area !== '' && <p>{ area }</p>
+              }
+              {
+                alcoholicOrNot !== '' && <p>{ alcoholicOrNot }</p>
+              }
+              <h2 data-testid={ `${i}-horizontal-name` }>{ name }</h2>
+              <p data-testid={ `${i}-horizontal-done-date` }>{ doneDate }</p>
+              <input
+                type="image"
+                src={ shared }
+                onClick={ () => handleShare(id, type) }
+                data-testid={ `${i}-horizontal-share-btn` }
+                alt="share-btn"
+              />
+              {
+                tags !== '' && tags.split(',').map((tag) => ((
+                  <p
+                    key={ tag }
+                    data-testid={ `${i}-${tag}-horizontal-tag` }
+                  >
+                    { tag }
+                  </p>
+                )))
+              }
+            </div>
+          ),
+        )
+      }
+    </div>
   );
 }
