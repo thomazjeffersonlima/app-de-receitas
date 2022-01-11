6b92edf2b005d1306d90944361d602162967471c
Guarize <pedroguarize@hotmail.com>
2022-01-11 16:57:25 -0300

    [feat] - req 65

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -7,6 +7,7 @@ import blackHeart from '../images/blackHeartIcon.svg';
 export default function FavoriteRecipes() {
   const [favoriteRecipes, setFavoriteRecipes] = useState([]);
   const [isCopied, setIsCopied] = useState(false);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     if (!JSON.parse(localStorage.getItem('favoriteRecipes'))) {
@@ -48,61 +49,75 @@ export default function FavoriteRecipes() {
       )}
       <section className="favorites-wrapper">
         <div>
-          <button type="button" data-testid="filter-by-all-btn">
+          <button
+            type="button"
+            onClick={ () => setFilter('') }
+            data-testid="filter-by-all-btn"
+          >
             All
           </button>
-          <button type="button" data-testid="filter-by-food-btn">
+          <button
+            type="button"
+            onClick={ () => setFilter('comida') }
+            data-testid="filter-by-food-btn"
+          >
             Foods
           </button>
-          <button type="button" data-testid="filter-by-drink-btn">
+          <button
+            type="button"
+            onClick={ () => setFilter('bebida') }
+            data-testid="filter-by-drink-btn"
+          >
             Drinks
           </button>
         </div>
         <div>
           {favoriteRecipes.length ? (
             <div>
-              {favoriteRecipes.map(
-                (
-                  { id, type, category, area, alcoholicOrNot, name, image },
-                  index,
-                ) => (
-                  <div key={ id }>
-                    <div>
-                      {type === 'comida' ? (
-                        <p data-testid={ `${index}-horizontal-top-text` }>
-                          {`${area} - ${category}`}
-                        </p>
-                      ) : (
-                        <p data-testid={ `${index}-horizontal-top-text` }>
-                          {alcoholicOrNot}
-                        </p>
-                      )}
-                      <p data-testid={ `${index}-horizontal-name` }>{name}</p>
+              {favoriteRecipes
+                .filter((recipe) => recipe.type.includes(filter))
+                .map(
+                  (
+                    { id, type, category, area, alcoholicOrNot, name, image },
+                    index,
+                  ) => (
+                    <div key={ id }>
                       <div>
-                        <input
-                          type="image"
-                          src={ shareIcon }
-                          alt="share icon"
-                          data-testid={ `${index}-horizontal-share-btn` }
-                          onClick={ () => handleShare(type, id) }
-                        />
-                        <input
-                          type="image"
-                          src={ blackHeart }
-                          alt="favorite icon"
-                          data-testid={ `${index}-horizontal-favorite-btn` }
-                          onClick={ () => handleFavorite(id) }
-                        />
+                        {type === 'comida' ? (
+                          <p data-testid={ `${index}-horizontal-top-text` }>
+                            {`${area} - ${category}`}
+                          </p>
+                        ) : (
+                          <p data-testid={ `${index}-horizontal-top-text` }>
+                            {alcoholicOrNot}
+                          </p>
+                        )}
+                        <p data-testid={ `${index}-horizontal-name` }>{name}</p>
+                        <div>
+                          <input
+                            type="image"
+                            src={ shareIcon }
+                            alt="share icon"
+                            data-testid={ `${index}-horizontal-share-btn` }
+                            onClick={ () => handleShare(type, id) }
+                          />
+                          <input
+                            type="image"
+                            src={ blackHeart }
+                            alt="favorite icon"
+                            data-testid={ `${index}-horizontal-favorite-btn` }
+                            onClick={ () => handleFavorite(id) }
+                          />
+                        </div>
                       </div>
+                      <img
+                        src={ image }
+                        alt="recipe preview"
+                        data-testid={ `${index}-horizontal-image` }
+                      />
                     </div>
-                    <img
-                      src={ image }
-                      alt="recipe preview"
-                      data-testid={ `${index}-horizontal-image` }
-                    />
-                  </div>
-                ),
-              )}
+                  ),
+                )}
             </div>
           ) : (
             <p>Nenhuma receita favorita.</p>
